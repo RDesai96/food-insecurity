6b86d8cded6f84b8dec1bfe7a4d55e8d1abc464f
Rajan <rajandes7@gmail.com>
2021-04-06 23:32:42 -0500

    More work on modeling. Having convergence problems, will revisit EDA and look at new approach.

--- preprocessing/EDA.py
+++ preprocessing/EDA.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 
 # Below is the code I used for EDA (Exploratory Data Analysis)
 # This dataset did not really require any cleaning, you can use the
@@ -13,6 +15,29 @@ df_labels = df.columns.to_list()
 
 # Removing CensusTract (it's just a numeric key) and County
 df.drop(['CensusTract','County'],axis=1,inplace=True)
+supervisors = ['LAPOP1_20','LAPOP1_10','LAPOP05_10','LALOWI1_10','LALOWI05_10','LALOWI1_20', 'lapophalf']
+
+# Plotting the potential supervisors.
+# All highly skewed to the right since most census tracts have <100 people
+def draw_histograms(df, variables, n_rows, n_cols, bin_size):
+    fig=plt.figure()
+    for i, var_name in enumerate(variables):
+        ax=fig.add_subplot(n_rows,n_cols,i+1)
+        df[var_name].hist(bins=bin_size,ax=ax)
+        ax.set_title(var_name+" Distribution")
+    fig.tight_layout()  # Improves appearance a bit.
+    plt.show()
+
+draw_histograms(df[supervisors], supervisors, 3, 3, 10)
+
+# Log transforming gets rid of skewness
+# Rounding up all population counts since fractional and
+# population counts <0 don't make sense before transforming
+df_log = df[supervisors].apply(np.rint,axis=1)
+df_log = np.log(df_log.replace(0,np.nan))
+df_log = df_log.fillna(0)
+draw_histograms(df_log, supervisors, 3, 3, 10)
+
 
 # Checking for missing values, there are no missing values
 print(df.isna().sum().sum())
--- preprocessing/modeling.py
+++ preprocessing/modeling.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.preprocessing import StandardScaler
@@ -8,11 +9,14 @@ from sklearn.compose import ColumnTransformer
 from sklearn.linear_model import ElasticNetCV
 import time
 
-# Potential supervisors
-supervisors = ['LAPOP1_10','LAPOP05_10','LAPOP1_20','LALOWI1_10',
-               'LALOWI05_10','LALOWI1_20', 'lapophalf']
 # Full dataset
 df = pd.read_csv('data/cleandata/FoodAccessCleaned.csv')
+
+# Potential supervisors
+# There some population counts that are 0 < pop < 1
+# Rounding up so we don't have negative values when we log transform the supervisors
+supervisors = ['LAPOP1_20','LAPOP1_10','LAPOP05_10','LALOWI1_10','LALOWI05_10','LALOWI1_20', 'lapophalf']
+df[supervisors] = df[supervisors].apply(np.rint,axis=1)
 
 # Dataset without potential supervisors
 df_learn = df[df.columns[~df.columns.isin(supervisors)]]
@@ -43,25 +47,30 @@ X_train, X_val, y_train, y_val =\
 
 
 transformer = ColumnTransformer([('minmax', MinMaxScaler() , quant_vars ),
-                                 ('cat'   , OneHotEncoder(), ['State']    ) ])
+                                 ('cat'   , OneHotEncoder(sparse=False), ['State']    ) ])
 
 # Grid for l1_ratio (i.e. the ratio of lasso:ridge)
 # I'm letting the default ElasticnetCV search figure out the amount of penalty
 # so I can't include 0 in the grid (otherwise estimation breaks down) so
 # I just chose a really small decimal
-l1_grid  = [0.0001,0.2,0.4,0.6,0.8,1]
+l1_grid  = [0.001,0.1,0.25,0.5,0.7,0.9,1]
 
 # n_jobs = -1 means use all cores in computer
-elastic_net = ElasticNetCV(l1_ratio=l1_grid, cv = 10, n_jobs=None)
+elastic_net = ElasticNetCV(l1_ratio=l1_grid, cv = 10,
+                           n_jobs=None, max_iter=100000)
 
 pipe = Pipeline( [('transform',  transformer),
                   ('elasticnet', elastic_net ) ])
 
-t0 = time.time()
-pipe.fit(X_val,y_val)
-t1 = time.time()
-total_time = t1-t0
+pipe.fit(X_val,np.log(y_val,where=(y_val != 0.0) ))
+val_coefs = pipe['elasticnet'].coef_
 
+pipe.set_params(elasticnet__alphas=[pipe[1].alpha_])
+pipe.set_params(elasticnet__l1_ratio=pipe[1].l1_ratio_)
 
-coefs = pipe[1].coef_
-pipe['transform'].transformers_[1][1].categories_
+pipe.fit(X_train,y_train)
+train_coefs = pipe['elasticnet'].coef_
+
+pipe.predict(X_test)
+print(pipe.score(X_test,y_test))
+
