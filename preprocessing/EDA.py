7ba2432b253aceb8d1b27ad246754dbfa6d03621
Rajan <rajandes7@gmail.com>
2021-04-08 01:06:27 -0500

    Resolved convergence issues. Improved EDA. Added Plots. Still unable to get feature names from sklearn transformer objects.

--- preprocessing/EDA.py
+++ preprocessing/EDA.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import numpy as np
+from matplotlib.backends.backend_pdf import PdfPages
 import matplotlib.pyplot as plt
+import seaborn as sns
 
 # Below is the code I used for EDA (Exploratory Data Analysis)
 # This dataset did not really require any cleaning, you can use the
@@ -17,9 +19,12 @@ df_labels = df.columns.to_list()
 df.drop(['CensusTract','County'],axis=1,inplace=True)
 supervisors = ['LAPOP1_20','LAPOP1_10','LAPOP05_10','LALOWI1_10','LALOWI05_10','LALOWI1_20', 'lapophalf']
 
-# Plotting the potential supervisors.
-# All highly skewed to the right since most census tracts have <100 people
-def draw_histograms(df, variables, n_rows, n_cols, bin_size):
+# df of just the features
+df_features = df[df.columns[~df.columns.isin(supervisors)]]
+
+# Plotting histograms of the data
+def draw_histograms(df, variables, n_rows, n_cols, bin_size=100):
+    """Draws a grid of 100 bin histograms with n_rows x n_cols"""
     fig=plt.figure()
     for i, var_name in enumerate(variables):
         ax=fig.add_subplot(n_rows,n_cols,i+1)
@@ -28,7 +33,31 @@ def draw_histograms(df, variables, n_rows, n_cols, bin_size):
     fig.tight_layout()  # Improves appearance a bit.
     plt.show()
 
-draw_histograms(df[supervisors], supervisors, 3, 3, 10)
+# Creating a pdf file with all the histograms for all the features
+def hist_pdf(df, filename='Feature_hists', bins=100):
+    """Creates a pdf file of 9 x 9 100 bin histograms on each page out of your feature df
+    and saves to filename"""
+    pp = PdfPages(filename + '.pdf')
+    ceil_hist = (df.shape[1] // 9)*9
+    if ceil_hist <= 9:
+        draw_histograms(df,df.columns,3,3,bin_size=bins)
+        pp.savefig()
+    else:
+    for i in range(9,df.shape[1]+9,9):
+        if i <= ceil_hist:
+            draw_histograms(df.iloc[:,(i-9):i], df.columns[(i-9):i], 3, 3, bin_size=bins)
+            pp.savefig()
+        elif i > ceil_hist:
+            i = df.shape[1]
+            diff = df.shape[1] - ceil_hist
+            draw_histograms(df.iloc[:,(i-diff):i], df.columns[(i-diff):i], 3, 3, bin_size=bins)
+            pp.savefig()
+    pp.close()
+hist_pdf(df_features, filename='preprocessing/Plots/Feature_hists')
+
+# histograms of the potential supervisors.
+# All highly skewed to the right since there are lot of tracts with ~0 people
+hist_pdf(df[supervisors],filename='preprocessing/Plots/Sup_hists')
 
 # Log transforming gets rid of skewness
 # Rounding up all population counts since fractional and
@@ -36,22 +65,55 @@ draw_histograms(df[supervisors], supervisors, 3, 3, 10)
 df_log = df[supervisors].apply(np.rint,axis=1)
 df_log = np.log(df_log.replace(0,np.nan))
 df_log = df_log.fillna(0)
-draw_histograms(df_log, supervisors, 3, 3, 10)
 
+# histograms of the transformed supervisors
+hist_pdf(df_log,filename='preprocessing/Plots/Sup_log_hists')
+
+# Correlation matrix with all the transformed supervisors
+def sborn_heatmap(df,filename='plot',title='Plot'):
+    """Plots and saves a seaborn heatmap to filename as a png"""
+    corr = df.corr()
+    mask = np.triu(np.ones_like(corr, dtype=bool))
+    plt.subplots(figsize=(11, 9))
+    cmap = sns.diverging_palette(230, 20, as_cmap=True)
+    sns.heatmap(corr, mask=mask, cmap=cmap, center=0,
+                square=True, linewidths=.5, cbar_kws={"shrink": .5})
+    plt.title(title)
+    plt.savefig(filename + '.png')
+sborn_heatmap(df_log,filename='preprocessing/Plots/sup_log_corrs',
+              title='Transformed Supervisors')
+
+# Correlation matrix with all the features
+sborn_heatmap(df_features,filename='preprocessing/Plots/feature_corrs',
+              title='Use FoodAccessLabel.csv as reference to identify unlabeled features')
 
 # Checking for missing values, there are no missing values
 print(df.isna().sum().sum())
 print(df.isnull().sum().sum())
 
 # Create a list of numeric vars and a list of all other dtypes
-quant_vars = df.select_dtypes(include=['int64','float64']).columns.to_list()
-factor_vars = df.select_dtypes(include=['datetime64','timedelta','object','category']).columns.to_list()
+def dtype_split(df):
+    """Returns a list of quantitative variables (pandas.dtype = int64,float64) and a list of
+     categorical variables (pandas.dtype = datetime64,timedelta,object,category)."""
+    quant_vars = df.select_dtypes(include=['int64','float64']).columns.to_list()
+    factor_vars = df.select_dtypes(include=['datetime64','timedelta','object','category']).columns.to_list()
+    return quant_vars, factor_vars
+quant_vars, factor_vars = dtype_split(df)
 
 # Examining possible categorical variables encoded as numeric by looking at columns with <10 unique values
-unique_counts = df[quant_vars].nunique().to_dict()
-sus_cat = [name for name,count in unique_counts.items() if count < 10]
-sus_num = [name for name,count in unique_counts.items() if count >= 10]
+def quant_examine(df, limit=10):
+    """Returns a list of suspected categorical vars and numeric vars
+    depending on if df.unique_counts > limit (default limit = 10) """
+    unique_counts = df[quant_vars].nunique().to_dict()
+    sus_cat = [name for name,count in unique_counts.items() if count < limit]
+    sus_num = [name for name,count in unique_counts.items() if count >= limit]
+    return sus_cat, sus_num
+sus_cat, sus_num = quant_examine(df[quant_vars])
 
-# Exporting df for modeling
+# Encoding new column of bins based on tract population
+# Will be useful later for stratified data splitting
+bin_labels = ['a','b','c','d','e','f','g','h','i','k']
+df['POP2010_bins'] = pd.cut(df['POP2010'],
+                            bins=[-np.inf,1000,2000,3000,4000,5000,6000,7000,8000,9000,np.inf],
+                            labels=bin_labels,retbins=False)
 df.to_csv('data/cleandata/FoodAccessCleaned.csv',sep=',', index=False)
-
--- preprocessing/modeling.py
+++ preprocessing/modeling.py
@@ -1,13 +1,13 @@
 import pandas as pd
 import numpy as np
+import math
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.preprocessing import StandardScaler
-from sklearn.preprocessing import MinMaxScaler
 from sklearn.pipeline import Pipeline
 from sklearn.compose import ColumnTransformer
 from sklearn.linear_model import ElasticNetCV
-import time
+from sklearn.metrics import mean_squared_error as mse
 
 # Full dataset
 df = pd.read_csv('data/cleandata/FoodAccessCleaned.csv')
@@ -16,7 +16,7 @@ df = pd.read_csv('data/cleandata/FoodAccessCleaned.csv')
 # There some population counts that are 0 < pop < 1
 # Rounding up so we don't have negative values when we log transform the supervisors
 supervisors = ['LAPOP1_20','LAPOP1_10','LAPOP05_10','LALOWI1_10','LALOWI05_10','LALOWI1_20', 'lapophalf']
-df[supervisors] = df[supervisors].apply(np.rint,axis=1)
+# df[supervisors] = df[supervisors].apply(np.rint,axis=1)
 
 # Dataset without potential supervisors
 df_learn = df[df.columns[~df.columns.isin(supervisors)]]
@@ -27,43 +27,55 @@ quant_vars = df_learn.select_dtypes(include=['int64','float64']).columns.to_list
 factor_vars = df.select_dtypes(include=['datetime64','timedelta','object','category']).columns.to_list()
 
 unique_counts = df[quant_vars].nunique().to_dict()
-sus_num = [name for name,count in unique_counts.items() if count >= 10]
-sus_cat = [name for name,count in unique_counts.items() if count < 10]
+sus_num = [name for name,count in unique_counts.items() if count > 10]
+sus_cat = [name for name,count in unique_counts.items() if count <= 10]
 
 # Final list of cat vars (factor_vars) and quantitative vars (quant_vars)
 factor_vars += sus_cat
 quant_vars = sus_num
 
-# TODO split into training and test set and then create pipeline object for all transformations
+# Split into train, val, and test sets using stratified by census tract population bins
+supervisor = df[supervisors[1]]
+# supervisor = np.log(supervisor, where=(supervisor != 0.0))
+
 X_train, X_test, y_train, y_test = \
-    train_test_split(df_learn, df[supervisors[0]],
+    train_test_split(df_learn, supervisor,
                      test_size=0.20,
-                     stratify=df_learn['State'])
+                     stratify=df_learn['POP2010_bins'])
 
 X_train, X_val, y_train, y_val =\
-    train_test_split(X_train, y_train,
-                     test_size=0.4,
-                     stratify=X_train['State'])
+    train_test_split(X_train, y_train, test_size=0.4,stratify=X_train['POP2010_bins'])
 
+# Dropping tract populations bins before proceeding with model fitting
+X_train = X_train.drop('POP2010_bins',axis=1).copy()
+X_val = X_val.drop('POP2010_bins',axis=1).copy()
+X_test = X_test.drop('POP2010_bins',axis=1).copy()
 
-transformer = ColumnTransformer([('minmax', MinMaxScaler() , quant_vars ),
-                                 ('cat'   , OneHotEncoder(sparse=False), ['State']    ) ])
+transformer = ColumnTransformer([('minmax', StandardScaler() , quant_vars ),
+                                 ('cat'   , OneHotEncoder(), ['State']    ) ])
+
 
 # Grid for l1_ratio (i.e. the ratio of lasso:ridge)
 # I'm letting the default ElasticnetCV search figure out the amount of penalty
 # so I can't include 0 in the grid (otherwise estimation breaks down) so
-# I just chose a really small decimal
+# I just chose a really small decimal in place of 0
 l1_grid  = [0.001,0.1,0.25,0.5,0.7,0.9,1]
 
 # n_jobs = -1 means use all cores in computer
 elastic_net = ElasticNetCV(l1_ratio=l1_grid, cv = 10,
-                           n_jobs=None, max_iter=100000)
+                           n_jobs=None, max_iter=100000, random_state=57)
 
 pipe = Pipeline( [('transform',  transformer),
                   ('elasticnet', elastic_net ) ])
 
-pipe.fit(X_val,np.log(y_val,where=(y_val != 0.0) ))
+# Converting all feature matrices to numpy arrays, as
+# recommended in scikit-learn doc to avoid unnecessary copying
+
+pipe.fit(X_val, y_val)
 val_coefs = pipe['elasticnet'].coef_
+
+print(pipe[1].alpha_)
+print(pipe[1].l1_ratio_)
 
 pipe.set_params(elasticnet__alphas=[pipe[1].alpha_])
 pipe.set_params(elasticnet__l1_ratio=pipe[1].l1_ratio_)
@@ -71,6 +83,16 @@ pipe.set_params(elasticnet__l1_ratio=pipe[1].l1_ratio_)
 pipe.fit(X_train,y_train)
 train_coefs = pipe['elasticnet'].coef_
 
-pipe.predict(X_test)
-print(pipe.score(X_test,y_test))
+y_pred = pipe.predict(X_test)
+print('Rsquared: ' + str(pipe.score(X_test,y_test)))
 
+MSE = mse(y_test,y_pred)
+RMSE = np.sqrt(MSE)
+print('MSE: ' + str(MSE) )
+print('RMSE: ' + str(RMSE))
+
+
+# MSE = (math.e**mse(y_test,y_pred))
+# RMSE = np.sqrt(MSE)
+# print('MSE: ' + str(MSE))
+# print('RMSE: ' + str(RMSE))
